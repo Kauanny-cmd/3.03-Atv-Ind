3481c8378320a6d4882ab070cf2244e0bf3bd209
kauanny-cmd <kauannyvieira8@gmail.com>
2023-04-14 14:00:05 -0300

    questão 3

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 import { quest1 } from "./q1";
 import { quest2 } from "./q2";
+import { quest3 } from "./q3";
 
 let a = ['kauanny', 'Wes', 'kaue']
 let b = ['kauanny', 'Wes', 'kaue']
+//let b = ['a', 'b']
 
-console.log(quest1({a, b}));
-console.log(quest2({a, b}));
+const array1 = ["abc", "def", "ghi"];
+const array2 = ["abc", "def", "ghij"];
+
+console.log(`Questão 1: ` + quest1({a, b})); // questão 1
+console.log(`Questão 2: ` + quest2({a, b})); // questao 2
+console.log(`Questão 3: ` + quest3(array1, array2)); //questao 3
---
+++ q3/index.ts
@@ -0,0 +1,9 @@
+export const quest3 = (arr1: string[], arr2: string[]): boolean =>{
+
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+  
+    return arr1.every((element, index) => element === arr2[index]);
+  }
+  
